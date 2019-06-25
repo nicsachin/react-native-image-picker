c4e1de6d0eb0dfb43db2804acda89efadc6037dc
nicsachin <nicsachin007@gmail.com>
2019-06-25 08:02:38 +0530

    first

---
+++ app/components/Hello.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { Text, View} from 'react-native';
+import styles from "./styles";
+
+
+export default class App extends Component{
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Hello world!!!</Text>
+      </View>
+    );
+  }
+}
